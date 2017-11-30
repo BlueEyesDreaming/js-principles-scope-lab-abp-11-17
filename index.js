18173486207b696cc8b21f63f5a17c044cbf204a
Ashton Reed <dreamergirl411@gmail.com>
2017-11-30 03:11:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var customerName = 'bob';
 function upperCaseCustomerName() {
   return customerName.toUpperCase()
 }
+console.log(customerName)
 //upperCaseCustomerName = customerName.toUpperCase()
 //function setBestCustomer(bestCustomer) {
 
